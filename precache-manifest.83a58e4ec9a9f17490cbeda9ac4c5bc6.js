03ebb1dec1af31f4b7d451f75b01e63da7848752
Iryna <irynka1071994@gmail.com>
2020-05-26 01:54:28 +0300

    Updates

---
+++ precache-manifest.83a58e4ec9a9f17490cbeda9ac4c5bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651523559fd3a411b2382370be871601",
+    "url": "/bookEvent/index.html"
+  },
+  {
+    "revision": "8a5b71c7d9420ff7bba7",
+    "url": "/bookEvent/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7b72be15cc5fd0c3d9c5",
+    "url": "/bookEvent/static/js/2.5de06b5b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bookEvent/static/js/2.5de06b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5b71c7d9420ff7bba7",
+    "url": "/bookEvent/static/js/main.f8e8eccc.chunk.js"
+  },
+  {
+    "revision": "ca8ddd598d5b3ad4752a",
+    "url": "/bookEvent/static/js/runtime-main.00d6430d.js"
+  }
+]);
